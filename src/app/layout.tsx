0a310a006e0a250498f4204c39e1a4a4fde79579
Eoin Logue <eoinlogue@Eoins-MacBook-Air.local>
2025-11-24 10:08:37 +0000

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "@/lib/userContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -26,6 +27,7 @@ export default function RootLayout({
       >
         <UserProvider>
           {children}
+          <Analytics />
         </UserProvider>
       </body>
     </html>
